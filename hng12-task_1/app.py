f7c885a8c071ec97aa11d7c5edc8990c21734123
AdetokunAdenike <adetokunadenike@gmail.com>
2025-02-04 12:48:54 +0100

    Update

--- hng12-task_1/app.py
+++ hng12-task_1/app.py
@@ -63,3 +63,6 @@ def classify_number():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
